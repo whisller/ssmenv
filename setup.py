24508566aefb3884709042b408519536ee3c8fab
Daniel Ancuta <whisller@gmail.com>
2018-07-04 15:29:52 +0100

    Change pipenv to requirements.txt

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
-import json
 import os
 from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md')) as f:
     README = f.read()
 
-with open('Pipfile.lock') as fd:
-    lock_data = json.load(fd)
-    install_requires = [
-        package_name + package_data['version']
-        for package_name, package_data in lock_data['default'].items()
-    ]
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name='ssm_or_env',
-    version='0.1',
+    version='0.3',
     description='Reads parameters from AWS SSM if in AWS context, otherwise returns Environment variables',
     long_description=README,
     packages=find_packages(),
